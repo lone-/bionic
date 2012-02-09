bd2cfed93d49a4db069f223e2df37d08619ab8c9
lonestar <xinz.06@gmail.com>
2012-02-09 09:26:04 +0800

    The Loongson kernel page is 16KB instead of 4KB in most system

--- libc/kernel/arch-mips/asm/page.h
+++ libc/kernel/arch-mips/asm/page.h
@@ -13,7 +13,7 @@
 #define _ASM_PAGE_H
 
 #ifndef PAGE_SHIFT
-#define PAGE_SHIFT 12
+#define PAGE_SHIFT 14
 #endif
 #define PAGE_SIZE (1UL << PAGE_SHIFT)
 #define PAGE_MASK (~((1 << PAGE_SHIFT) - 1))
